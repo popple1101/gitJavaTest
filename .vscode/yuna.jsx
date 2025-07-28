01257f9f6d45032c900b727f412ffad1d1e6eecd
yuna <yuna5470@naver.com>
2025-07-28 14:55:54 +0900

    테스트

--- .vscode/yuna.jsx
+++ .vscode/yuna.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function yuna() {
   return (
-    <div><h2>우리 모두 화이팅 화아ㅣㅓ라ㅣㅓㅇㄴ</h2></div>
+    <div><h2>우리 모두 화이팅 화아ㅣㅓ라ㅣㅓㅇㄴ</h2>
+    <h2>밥을 먹어도 배고프네요</h2>
+    </div>
   )
 }
